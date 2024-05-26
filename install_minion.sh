4be33781069ea71e894019e5edfa4b540a9db659
khaddict <ab20meki@gmail.com>
2024-05-27 00:17:46 +0200

    Delete install_minion.sh

--- install_minion.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/debian/12/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/debian/12/amd64/latest bookworm main" | sudo tee /etc/apt/sources.list.d/salt.list
-apt update
-apt install salt-minion
-echo "master: saltmaster.homelab.lan" > /etc/salt/minion
-systemctl enable salt-minion && systemctl start salt-minion
