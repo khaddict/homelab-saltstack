dcc15409be68b296c837b1a172b162dd71a7cebf
khaddict <139250194+khaddict@users.noreply.github.com>
2024-06-14 21:25:47 +0200

    Update post_deploy.sh

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -88,8 +88,9 @@ elif [ "$ARCH" = "aarch64" ]; then
     ARCH="arm64"
 fi
 mkdir -p /etc/apt/keyrings
+apt install curl
 curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/$DISTRO/$VERSION/$ARCH/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/$DISTRO/$VERSION/$ARCH/latest $VERSION_CODENAME main" | tee /etc/apt/sources.list.d/salt.list
+echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=$ARCH] https://repo.saltproject.io/salt/py3/$DISTRO/$VERSION/$ARCH/latest $VERSION_CODENAME main" | tee /etc/apt/sources.list.d/salt.list
 apt update
 echo $DONE_MSG
 
